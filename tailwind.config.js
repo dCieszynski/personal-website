72cdaf54cc6d04d96b13914da5b2096bbbdcf9d7
Dawid <dcieszynski99@gmail.com>
2023-04-17 21:22:21 +0200

    Add Home page animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        imb: ["IBM Plex Mono", "sans-serif"],
+        imb: ["IBM Plex Mono", "monospace"],
       },
       boxShadow: {
         brutal: "4px 4px 0px #000",
@@ -14,7 +14,43 @@ export default {
         "personal-orange-2": "#ffce2f",
         "personal-orange-1": "#ffdc69",
       },
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden",
+          },
+          "100%": {
+            width: "100%",
+          },
+        },
+        "move-from-right": {
+          "0%": {
+            transform: "translateX(100%)",
+            opacity: 0,
+          },
+          "100%": {
+            transform: "translateX(0%)",
+            opacity: 1,
+          },
+        },
+        "move-from-left": {
+          "0%": {
+            transform: "translateX(-100%)",
+            opacity: 0,
+          },
+          "100%": {
+            transform: "translateX(0%)",
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        typing: "typing 2s steps(40, end) alternate, .7s",
+        "move-from-right": "move-from-right 1s ease-out",
+        "move-from-left": "move-from-left 1s ease-out",
+      },
     },
+    plugins: [],
   },
-  plugins: [],
 };
